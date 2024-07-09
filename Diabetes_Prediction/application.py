7b1214ee8640e61eadcf7c34e06eae69d2d1b3a3
Gthejesraj <thejes04@gmail.com>
2024-07-09 12:24:11 +0530

    End to end project which predicts diabetes

--- Diabetes_Prediction/application.py
+++ Diabetes_Prediction/application.py
@@ -8,10 +8,8 @@ import pandas as pd
 application = Flask(__name__)
 app = application
 
-scaler = pickle.load(open(
-    "/Users/thejesraj/Desktop/Projects/Diabetes_Prediction/Model/standardscaler.pkl", "rb"))
-model = pickle.load(open(
-    "/Users/thejesraj/Desktop/Projects/Diabetes_Prediction/Model/modelForPrediction.pkl", "rb"))
+scaler = pickle.load(open("Model/standardScalar.pkl", "rb"))
+model = pickle.load(open("Model/modelForPrediction.pkl", "rb"))
 
 # Route for homepage
 
